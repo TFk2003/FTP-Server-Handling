b6e59fe4cae7eba656b007eb7d1ac88fb95b26ce
Talha Farooq Khan <farooqtalha4@gmail.com>
2025-07-08 01:02:28 +0500

    secure security keys and password

--- src/main/java/org/example/cnproject/CnProjectApplication.java
+++ src/main/java/org/example/cnproject/CnProjectApplication.java
@@ -1,5 +1,6 @@
 package org.example.cnproject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CnProjectApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        System.setProperty("FTP_USERNAME", dotenv.get("FTP_USERNAME"));
+        System.setProperty("FTP_PASSWORD", dotenv.get("FTP_PASSWORD"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+        System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
         SpringApplication.run(CnProjectApplication.class, args);
     }
 
